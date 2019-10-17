728f82a113b0bf6bee324a558139d63d8ba7e630
Jens Tschirpig <jens1101@users.noreply.github.com>
2019-10-17 23:01:49 +0200

    Added function to fetch Gists
    - This async function will resolve into an array of gists
    - Added type definitions for these new objects

--- js/main.js
+++ js/main.js
@@ -10,6 +10,21 @@
  */
 
 /**
+ * @typedef {Object} Gist
+ * @property {string} description
+ * @property {GistFile} mainFile
+ * @property {URL} url
+ */
+
+/**
+ * @typedef {Object} GistFile
+ * @property {string} filename
+ * @property {string} language
+ * @property {string} languageClass
+ * @property {string} content
+ */
+
+/**
  * Gets all the pinned repos from GitHub for the specified user.
  * @param {string} username
  * @returns {Promise<Repository[]>}
@@ -25,4 +40,60 @@ async function getPinnedRepos (username) {
   }
 
   return await result.json()
+}
+
+/**
+ * Fetches all the public GitHub Gists of the specified user
+ * @param {string} user The username of the user
+ * @param {number} [page=1] The page offset
+ * @param {number} [perPage=10] The number of Gists per page
+ * @returns {Promise<Gist[]>} Resolves in an array of `Gist` objects
+ */
+async function getGists (user, page = 1, perPage = 10) {
+  const url = new URL(`https://api.github.com/users/${user}/gists`)
+  url.searchParams.append('page', `${page}`)
+  url.searchParams.append('per_page', `${perPage}`)
+
+  // Fetch the gists
+  const result = await fetch(url.toString(), {
+    method: 'GET',
+    headers: new Headers({
+      'Accept': 'application/vnd.github.v3+json'
+    })
+  })
+
+  if (!result.ok) {
+    throw new Error('Could not retrieve pinned repos')
+  }
+
+  // Get the raw results from the API call. This array of objects contains too
+  // much data and needs to be manipulated further.
+  const rawGists = await result.json()
+
+  /**
+   * Contains an array of promises that each will resolve into a gist in the
+   * form that we want it in.
+   * @type Promise<Gist>[]
+   */
+  const gistsPromises = rawGists.map(gist => {
+    // Get the details of the first file in the gist
+    const mainFile = gist.files[Object.keys(gist.files).pop()]
+
+    // Fetch the contents of the main file and then resolve this gist as a valid
+    // `Gist` object
+    return fetch(mainFile.raw_url)
+      .then(blob => blob.text())
+      .then(content => ({
+        description: gist.description,
+        mainFile: {
+          filename: mainFile.filename,
+          language: mainFile.language,
+          languageClass: `language-${mainFile.language.toLowerCase()}`,
+          content
+        },
+        url: new URL(gist.url)
+      }))
+  })
+
+  return await Promise.all(gistsPromises)
 }
