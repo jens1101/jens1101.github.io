1eaa231c9cf434cc1fcafb87b7ac78509aa48815
Jens Tschirpig <jens1101@users.noreply.github.com>
2019-10-30 22:31:03 +0200

    Added documentation and moved functions around

--- js/main.js
+++ js/main.js
@@ -25,7 +25,6 @@
  */
 
 // TODO: if an AJAX error occurs then remove the loading elements
-// TODO: update documentation
 
 (async function main () {
   /**
@@ -55,30 +54,95 @@
   ])
 })()
 
+/**
+ * Loads all the public pinned repos for the given GitHub user into the home
+ * page
+ * @param {string} githubUsername The username to query
+ * @param {string} githubToken The token to use for the query
+ * @param {number} limit The number of pinned repos to get
+ * @returns {Promise<void>}
+ */
 async function loadPinnedRepos (githubUsername, githubToken, limit) {
+  /**
+   * All the card elements that will be populated
+   * @type {HTMLElement[]}
+   */
   const repoCardElements = initCards('#repo-card-template',
     '#my-pinned-repos', limit)
-
-  // Get all the Gists
+  /**
+   * All of the pinned repositories that need to be loaded into the card
+   * elements
+   * @type {Repository[]}
+   */
   const repos = await getPinnedRepos(githubUsername, githubToken, limit)
 
   fillElements(repoCardElements, repos, (repoCardElement, repo) => {
-    /**
-     * @type {HTMLAnchorElement}
-     */
+    // Add the repository name and link
+    /** @type {HTMLAnchorElement} */
     const titleLink = repoCardElement.querySelector('.card-title__link')
     titleLink.textContent = repo.name
     titleLink.href = repo.url.toString()
 
+    // Add the repository description
     repoCardElement.querySelector('.card-text__description')
       .textContent = repo.description
 
+    // Add the repository's language shield
     repoCardElement
       .querySelector('.card-text__language')
       .src = `https://img.shields.io/github/languages/top/${githubUsername}/${repo.name}`
 
     // Remove the "loading" class
     repoCardElement.classList.remove('card--loading')
+  })
+}
+
+/**
+ * Loads all the public gists for the given GitHub user into the home page
+ * @param {string} githubUsername The username to query
+ * @param {string} githubToken The token to use for the query
+ * @param {number} limit The number of gists to get
+ * @returns {Promise<void>}
+ */
+async function loadGists (githubUsername, githubToken, limit) {
+  /**
+   * All the card elements that will be populated
+   * @type {HTMLElement[]}
+   */
+  const gistCardElements = initCards('#gist-card-template', '#my-gists', limit)
+  /**
+   * All of the gists that need to be loaded into the card elements
+   * @type {Gist[]}
+   */
+  const gists = await getGists(githubUsername, githubToken, limit)
+
+  fillElements(gistCardElements, gists, (gistCardElement, gist) => {
+    // Remove all children of the code element. This is to prevent unexpected
+    // whitespace in the code preview.
+    const codeElement = gistCardElement.querySelector('.card-img-top code')
+    while (codeElement.firstChild) {
+      codeElement.firstChild.remove()
+    }
+
+    // Add the code text and highlight it.
+    codeElement.appendChild(document.createTextNode(gist.files[0].text))
+    codeElement.classList.add(
+      `language-${gist.files[0].language.name.toLowerCase()}`)
+    Prism.highlightElement(codeElement)
+
+    // Remove the "loading" class
+    gistCardElement.classList.remove('card--loading')
+
+    // Name of the main file that acts as the Gist title
+    gistCardElement.querySelector('.card-title')
+      .textContent = gist.files[0].name
+
+    // Gist description
+    gistCardElement.querySelector('.card-text')
+      .textContent = gist.description
+
+    // Gist link
+    gistCardElement.querySelector('.card-link').href = gist.url
   })
 }
 
@@ -142,41 +206,6 @@ function initCards (templateSelector, targetSelector, numberOfClones) {
   return cardElements
 }
 
-async function loadGists (githubUsername, githubToken, limit) {
-  const gistCardElements = initCards('#gist-card-template', '#my-gists', limit)
-
-  // Get all the Gists
-  const gists = await getGists(githubUsername, githubToken, limit)
-
-  fillElements(gistCardElements, gists, (gistCardElement, gist) => {
-    // Remove all children of the code element. This is to prevent unexpected
-    // whitespace in the code preview.
-    const codeElement = gistCardElement.querySelector('.card-img-top code')
-    while (codeElement.firstChild) {
-      codeElement.firstChild.remove()
-    }
-
-    codeElement.appendChild(document.createTextNode(gist.files[0].text))
-    codeElement.classList.add(
-      `language-${gist.files[0].language.name.toLowerCase()}`)
-    Prism.highlightElement(codeElement)
-
-    // Remove the "loading" class
-    gistCardElement.classList.remove('card--loading')
-
-    // Name of the main file that acts as the Gist title
-    gistCardElement.querySelector('.card-title')
-      .textContent = gist.files[0].name
-
-    // Gist description
-    gistCardElement.querySelector('.card-text')
-      .textContent = gist.description
-
-    // Gist link
-    gistCardElement.querySelector('.card-link').href = gist.url
-  })
-}
-
 /**
  * Iterates over the data and elements arrays simultaneously. A callback is
  * called on each iteration with the current element and data being sent as
@@ -191,7 +220,7 @@ async function loadGists (githubUsername, githubToken, limit) {
  * @param {HTMLElement[]} elements The array of elements to loop through.
  * @param {Array<T>} dataArray The array of data to loop through.
  * @param {function(HTMLElement, T)} callback Triggers during each iteration.
- *   The current element and data is passed as arguments.
+ * The current element and data is passed as arguments.
  */
 function fillElements (elements, dataArray, callback) {
   for (const data of dataArray) {
@@ -211,10 +240,10 @@ function fillElements (elements, dataArray, callback) {
 }
 
 /**
- * Gets all the pinned repos from GitHub for the specified user.
- * @param {string} githubUsername
- * @param {string} githubToken
- * @param {number} limit
+ * Gets all the public pinned repos from GitHub for the specified user.
+ * @param {string} githubUsername The username to query
+ * @param {string} githubToken The token to use for the query
+ * @param {number} limit The number of pinned repos to get
  * @returns {Promise<Repository[]>}
  */
 async function getPinnedRepos (githubUsername, githubToken, limit) {
@@ -238,23 +267,6 @@ async function getPinnedRepos (githubUsername, githubToken, limit) {
 
   // noinspection JSUnresolvedVariable
   return result.data.user.pinnedItems.edges.map(edge => edge.node)
-}
-
-async function callGithubApi (token, query, errorMessage) {
-  const result = await fetch('https://api.github.com/graphql', {
-    method: 'POST',
-    headers: {
-      'Content-Type': 'application/json',
-      'Authorization': `bearer ${token}`
-    },
-    body: JSON.stringify({ query })
-  })
-
-  if (!result.ok) {
-    throw new Error(errorMessage || 'Error while fetching data from server')
-  }
-
-  return await result.json()
 }
 
 /**
@@ -292,4 +304,30 @@ async function getGists (githubUsername, githubToken, limit) {
 
   // noinspection JSUnresolvedVariable
   return result.data.user.gists.edges.map(edge => edge.node)
+}
+
+/**
+ * Calls the GitHub GraphQL API
+ * @param {string} token The token to use to access the API
+ * @param {string} query The GraphQL query to run
+ * @param {string} [errorMessage] The error message to return if an error
+ * occurs. If omitted then a generic error message will be returned instead
+ * @returns {Promise<Object>} The decoded response from the server
+ * @throws Error when the API couldn't be queried
+ */
+async function callGithubApi (token, query, errorMessage) {
+  const result = await fetch('https://api.github.com/graphql', {
+    method: 'POST',
+    headers: {
+      'Content-Type': 'application/json',
+      'Authorization': `bearer ${token}`
+    },
+    body: JSON.stringify({ query })
+  })
+
+  if (!result.ok) {
+    throw new Error(errorMessage || 'Error while fetching data from server')
+  }
+
+  return await result.json()
 }
